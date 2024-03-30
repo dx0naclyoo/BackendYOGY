6c20d988d40ce44b4cc94fe0b6c42e5cb5ba616c
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-03-30 12:37:39 +0300

    create models for auth

--- src/backend/api/auth.py
+++ src/backend/api/auth.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.backend.models import auth as models
+
 router = APIRouter(tags=["Auth"], prefix="/auth")
 
 
-@router.get("/")
+@router.get("/", response_model=models.User)
 async def user():
     return "user"
 
---
+++ src/backend/models/auth.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class Role(BaseModel):
+    id: int
+    name: str
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserRegister(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    role: Role
+
+    class Config:
+        from_attributes = True
