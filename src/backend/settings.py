1ef85007e56915b665680e581a8e3e0da956393b
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-04-01 16:33:04 +0300

    create dockerfile and compose up

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     port: int = 10000
     debug: bool = False
 
-    database_url: str = os.getenv("postgres_url")
+    database_url: str = "postgresql+asyncpg://default:CY3DZQ2PlHJO@ep-crimson-fog-a2l1kp2b-pooler.eu-central-1.aws.neon.tech:5432"
     database_echo: bool = True
 
     private_key_path: Path = BASE_DIR / "src" / "certs" / "jwt-private.pem"
