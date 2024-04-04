e0c2e232d1096831845f1d96f34874b1ff3afc91
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-04-05 02:21:20 +0300

    add cors

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,31 +1,28 @@
 from fastapi import FastAPI
 from .settings import settings
 from .api import router as api_v1
-from fastapi.middleware.cors import CORSMiddleware
-
-# docs_url=None, redoc_url=None | Settings for deployment on prod
-app = FastAPI(title="BackendYOGU", debug=settings.debug)
-app.include_router(api_v1)
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 
 origins = [
     "http://localhost:5173/",
     "http://127.0.0.1:5173/",
-    "https://localhost:5173/",
-    "https://127.0.0.1:5173/",
-    "10.217.154.102:0/",
-    "http://10.217.154.102:0/",
-    "https://10.217.154.102:0/",
-    "10.217.154.9:0/",
-    "http://10.217.154.9:0/",
-    "https://ugor.netlify.app/",
-    "https://ugor.netlify.app/auth"
+    "https://ugor.netlify.app/auth",
+    "https://ugor.netlify.app/"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins)
+]
+
+app = FastAPI(
+    title="BackendYOGU",
+    description="API For YOGY university",
+    debug=settings.debug,
+    middleware=middleware
 )
+# docs_url=None, redoc_url=None | Settings for deployment on prod
+
+
+app.include_router(api_v1)
