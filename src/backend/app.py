985ee1aa88bdb4ad526d85537abd74a1d89e600a
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-04-05 02:31:51 +0300

    add cors

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,28 +1,49 @@
-from fastapi import FastAPI
+from typing import Callable
+
+from fastapi import FastAPI, APIRouter
+from fastapi.routing import APIRoute
+from starlette.requests import Request
+from starlette.responses import Response
+
 from .settings import settings
 from .api import router as api_v1
-from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-
-
-origins = [
-    "http://localhost:5173/",
-    "http://127.0.0.1:5173/",
-    "https://ugor.netlify.app/auth",
-    "https://ugor.netlify.app/"
-]
-
-middleware = [
-    Middleware(CORSMiddleware, allow_origins=origins)
-]
 
 app = FastAPI(
     title="BackendYOGU",
     description="API For YOGY university",
     debug=settings.debug,
-    middleware=middleware
+
 )
 # docs_url=None, redoc_url=None | Settings for deployment on prod
-
-
 app.include_router(api_v1)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Handle CORS
+class CORSHandler(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def preflight_handler(request: Request):
+            if request.method == 'OPTIONS':
+                response = Response()
+                response.headers['Access-Control-Allow-Origin'] = '*'
+                response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+                response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+            else:
+                response = await original_route_handler(request)
+
+        return preflight_handler
+
+router = APIRouter(route_class=CORSHandler)
+
+app.include_router(router)
