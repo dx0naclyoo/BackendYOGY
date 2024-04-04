fd1b24dfc5ca2652ab027f047b80f43e2d85a935
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2024-04-04 23:16:26 +0500

    не работает get, должным образом 2

--- src/backend/api/orders.py
+++ src/backend/api/orders.py
@@ -10,20 +10,22 @@ from src.backend.services.auth import services as auth_services
 router = APIRouter(tags=["Orders"], prefix="/orders")
 
 
-@router.get("/{offset}", response_model=list[orders_models.Orders])
+@router.get("/all/{offset}", response_model=list[orders_models.Orders])
 async def get_orders_all(
         offset: int,
-        session: AsyncSession = Depends(databaseHandler.get_session)
+        session: AsyncSession = Depends(databaseHandler.get_session),
+        user_data: auth_models.User = Depends(auth_services.get_current_user),
 ):
-    return await orders_services.get_all(session=session, offset=offset)
+    return await orders_services.get_all(session=session, offset=offset, user_data=user_data)
 
 
 @router.get("/{order_id}", response_model=orders_models.Orders)
 async def get_order(
         order_id: int,
-        session: AsyncSession = Depends(databaseHandler.get_session)
+        session: AsyncSession = Depends(databaseHandler.get_session),
+        user_data: auth_models.User = Depends(auth_services.get_current_user),
 ):
-    return await orders_services.get(session=session, order_id=order_id)
+    return await orders_services.get(session=session, order_id=order_id, user_data=user_data)
 
 
 @router.post("/")
